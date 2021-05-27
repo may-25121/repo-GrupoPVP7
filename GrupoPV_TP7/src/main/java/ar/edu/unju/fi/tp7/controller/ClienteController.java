990635ce2ed84d1446faf2a0fad8115223b35251
AndreaFlorencia <andreabejarano629@gmail.com>
2021-05-27 16:15:16 -0300

    task:Realice el controller,pero me falta terminar de hacer  el de borrar.

--- GrupoPV_TP7/src/main/java/ar/edu/unju/fi/tp7/controller/ClienteController.java
+++ GrupoPV_TP7/src/main/java/ar/edu/unju/fi/tp7/controller/ClienteController.java
@@ -1,11 +1,14 @@
 package ar.edu.unju.fi.tp7.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -30,7 +33,19 @@ public class ClienteController {
 	@GetMapping("/cliente/nuevo")
 	public String getNuevoClientePage(Model model) {
 		model.addAttribute("cliente",cliente);
-		return "nuevocliente";
+		return "form-cliente";
+	}
+	@GetMapping("/cliente/modificar/{id}")
+	public String modificar(@PathVariable Long id,Model model ) {
+		List<Cliente>cliente = clienteService.getClientes();
+		model.addAttribute("clientes", cliente );
+		return  "form-cliente";
+		
+	}
+	@GetMapping("/cliente/borrar/{id}")
+	public String borrar(@PathVariable Long id,Model model ) {
+		
+		return  "clientes";
 	}
 	
 	@PostMapping("/cliente/guardar")
